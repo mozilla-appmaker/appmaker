84d65526e6d78a0f341c6411f76dfc900f51fa1d
Breck <breck7@gmail.com>
2013-08-02 15:08:15 -0700

    Added components

---
+++ public/components/components.js
@@ -0,0 +1,41 @@
+var Components = {}
+Components.tags = []
+Components.templates = {}
+
+Components.add = function (tagName) {
+  Components.tags.push(tagName)
+  Components.templates[tagName] = $('template#' + tagName).html().trim()
+}
+
+
+Components.broadcast = function (message) {
+  $('.component').trigger('dblclick', message)
+  Components.log(message)
+}
+
+Components.log = function (message) {
+  console.log(message)
+}
+
+// Replace all Component tags with the components
+Components.replace = function () {
+  Components.scan()
+  Components.tags.forEach(function (tagName, index) {
+    $(tagName).each(function () {
+      var clone = $(Components.templates[tagName])
+      clone.addClass('component')
+      clone.addClass(tagName)
+      $(this).replaceWith(clone)
+    })
+  })
+}
+
+Components.scan = function () {
+  // Scan the page for components
+  $('template').each(function () {
+    var id = $(this).attr('id')
+    Components.add(id)
+  })
+}
+
+
---
+++ public/components/readme.md
@@ -0,0 +1,5 @@
+Component
+=========
+
+A simple component system
+
