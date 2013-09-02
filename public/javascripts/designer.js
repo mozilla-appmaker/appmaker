cae519564aa449a7b5dfffe332b7a1a638f1f480
Simon Wex <simon@simonwex.com>
2013-09-02 01:15:52 +0100

    Fixing time formatting

--- public/javascripts/designer.js
+++ public/javascripts/designer.js
@@ -135,7 +135,14 @@ define(
       localStorage.draft = app.serialize();
 
       var now = new Date();
-      now = now.getHours() + ':' + now.getMinutes() + ":" + now.getSeconds();
+      var hours = now.getHours();
+      var minutes = now.getMinutes();
+      var seconds = now.getSeconds();
+      hours = (hours < 10) ? "0" + hours : hours;
+      minutes = (minutes < 10) ? "0" + minutes : minutes;
+      seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+      now = hours + ':' + minutes + ":" + seconds;
 
       $('.note').show();
       $('#time').text(now);
