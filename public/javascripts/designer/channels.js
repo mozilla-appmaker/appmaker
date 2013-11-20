1650b416275397865cd8ea6d7343c4b1d97528bf
Simon Wex <simon@simonwex.com>
2013-11-19 22:55:31 -0800

    Adding channels to designer

--- public/javascripts/designer/channels.js
+++ public/javascripts/designer/channels.js
@@ -8,9 +8,20 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 define(
-  ["jquery"],
-  function($) {
+  ["/ceci/ceci-designer.js"],
+  function(Ceci) {
     "use strict";
+
+    Ceci.addChannels([
+      {id: 'blue',   name: 'Blue Moon',          hex: '#358CCE'},
+      {id: 'red',    name: 'Red Baloon',         hex: '#e81e1e'},
+      {id: 'pink',   name: 'Pink Heart',         hex: '#e3197b'},
+      {id: 'purple', name: 'Purple Horseshoe',   hex: '#9f27cf'},
+      {id: 'green',  name: 'Green Clover',       hex: '#71b806'},
+      {id: 'yellow', name: 'Yellow Pot of Gold', hex: '#e8d71e'},
+      {id: 'orange', name: 'Orange Star',        hex: '#ff7b00'},
+      {id: null,     name: 'Disabled',           hex: '#444'},
+    ]);
 
   }
 );
