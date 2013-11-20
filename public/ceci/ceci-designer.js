83c618f211b0f2e1a384e3d8b58b7e1bde117808
Simon Wex <simon@simonwex.com>
2013-11-19 22:55:18 -0800

    Adding Channels and iterators to ceci-designer

--- public/ceci/ceci-designer.js
+++ public/ceci/ceci-designer.js
@@ -15,7 +15,28 @@ define([], function() {
     "ceci-listen"
   ];
 
-  return {
+  // Because of the way requirejs works, this is a singleton.
+  var CeciDesigner = {
+    // It's expected that the channel object being sent has a .id to uniquely identify it.
+    addChannels: function(channels){
+      var self = this;
+      Array.prototype.forEach.call(channels, function(channel) {
+        self.addChannel(channel);
+      });
+    },
+    addChannel: function(channel){
+      if (typeof channel !== 'object' || !("id" in channel)){
+        throw new TypeError("Channel \"id\" missing.");
+      }
+      this.channels[channel.id] = channel;
+      return this;
+    },
+    forEachChannel: function(fn){
+      for (var x in this.channels){
+        fn(this.channels[x]);
+      }
+      return this;
+    },
     getRegisteredComponents: function(){
       var components = [];
       for (var tagName in window.CustomElements.registry){
@@ -24,6 +45,17 @@ define([], function() {
         }
       }
       return components;
+    },
+    forEachComponent: function(fn){
+      var components = this.getRegisteredComponents();
+      for (var x in components){
+        fn(components[x].name, components[x]);
+      }
+      return this;
     }
   };
+
+  CeciDesigner.channels = {};
+
+  return CeciDesigner
 });
