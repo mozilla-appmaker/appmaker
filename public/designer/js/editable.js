da0c9d555027e63aac31b3af3a7baf48cfd0aba8
Simon Wex <simon@simonwex.com>
2014-01-27 21:56:25 -0800

    Unreferenced modules can just be left 'till last.

--- public/designer/js/editable.js
+++ public/designer/js/editable.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-define(['inflector', 'colorpicker.core', 'l10n'], function (Inflector, ColorPickerDummy, L10n) {
+define(['inflector', 'l10n', 'colorpicker.core'], function (Inflector, L10n) {
   var urlComponent = window.CustomElements;
   var editableTypeHandlers = {
     'multiple': function (element, attributeName, title, value, definition) {
