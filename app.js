50c118d070a5df8462ddd895d9a2e35a4471c123
Ali Al Dallal <ali@alicoding.com>
2014-04-11 11:44:14 -0400

    Update Dependencies

--- app.js
+++ app.js
@@ -227,7 +227,7 @@ if (!module.parent) {
     app.locals.components = components;
     localeBuild(components, i18n.getSupportLanguages(), function(map) {
       i18n.addLocaleObject(map, function(err, bool) {
-        if(bool) {
+        if(!err) {
           http.createServer(app).listen(app.get('port'), function(){
             console.log("Express server listening on port " + app.get('port'));
           });
