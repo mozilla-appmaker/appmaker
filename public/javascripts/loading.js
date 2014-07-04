50aba28da6e5e0ee7c11cacb8e4828cb951f39f5
scottdowne <scott.downe@gmail.com>
2014-07-04 13:44:23 -0400

    Fixes to loading

--- public/javascripts/loading.js
+++ public/javascripts/loading.js
@@ -1,25 +1,39 @@
 (function() {
   var loadingCover = document.querySelector(".loading-cover");
+  var steps = 5;
+  var currentWeight = 100;
+  var stepWeight = currentWeight / steps;
+
+  function step() {
+    currentWeight -= stepWeight;
+    loadingCover.style.width = currentWeight + "%";
+  }
+
   function onReadyStateComplete() {
     if (document.readyState === "complete") {
       document.removeEventListener("readystatechange", onReadyStateComplete);
-      loadingCover.style.width = "75%";
+      step();
     }
+  }
+  function onCeciAppCreated() {
+    window.removeEventListener("CeciAppCreated", onCeciAppCreated);
+    step();
   }
   function onCeciAppReady() {
     window.removeEventListener("CeciAppReady", onCeciAppReady);
-    loadingCover.style.width = "50%";
+    step();
   }
   function onCeciAppAttached() {
     window.removeEventListener("CeciAppAttached", onCeciAppAttached);
-    loadingCover.style.width = "25%";
+    step();
   }
-  function onPolymerReady() {
-    window.removeEventListener("polymer-ready", onPolymerReady);
-    loadingCover.style.width = "0%";
+  function onCeciAppDOMReady() {
+    window.removeEventListener("CeciAppDOMReady", onCeciAppDOMReady);
+    step();
   }
   document.addEventListener("readystatechange", onReadyStateComplete);
+  window.addEventListener("CeciAppCreated", onCeciAppCreated);
   window.addEventListener("CeciAppReady", onCeciAppReady);
   window.addEventListener("CeciAppAttached", onCeciAppAttached);
-  window.addEventListener("polymer-ready", onPolymerReady);
+  window.addEventListener("CeciAppDOMReady", onCeciAppDOMReady);
 }());
