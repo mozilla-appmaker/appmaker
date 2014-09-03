2c762799d89010a7b44b84d2fee88cb93fbeffb3
Pomax <pomax@nihongoresources.com>
2014-09-03 15:56:17 -0700

    loop detection only, no user alert or intervention

--- public/designer/js/channels.js
+++ public/designer/js/channels.js
@@ -3,20 +3,136 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 define(
-  ["ceci/ceci-designer"],
-  function(Ceci) {
+  function() {
     "use strict";
 
-    Ceci.addChannels([
-      {id: 'blue',   name: 'Blue Moon',          hex: '#358CCE'},
-      {id: 'red',    name: 'Red Baloon',         hex: '#e81e1e'},
-      {id: 'pink',   name: 'Pink Heart',         hex: '#e3197b'},
-      {id: 'purple', name: 'Purple Horseshoe',   hex: '#9f27cf'},
-      {id: 'green',  name: 'Green Clover',       hex: '#71b806'},
-      {id: 'yellow', name: 'Yellow Pot of Gold', hex: '#e8d71e'},
-      {id: 'orange', name: 'Orange Star',        hex: '#ff7b00'},
-      {id: null,     name: 'Disabled',           hex: '#444'},
-    ]);
+    // internal map for tracking the connection graph for an app
+    var maps = {
+      listen: {},
+      broadcast: {}
+    };
+
+    /**
+     * generic FindLoop wrapper.
+     */
+    function findDirectionalLoop(channel, original, direction, seen) {
+      var tracker = (direction === "forward" ? maps.listen : maps.broadcast),
+          fn = (direction === "forward" ? "getActiveBroadcasts" : "getActiveListeners");
+      function FindLoop(channel) {
+        if(!channel) return false;
+        var paths = tracker[channel] || [],
+            next = [], node, i, j;
+        for(i=paths.length-1; i>=0; i--) {
+          node = paths[i];
+          if(node === original) { seen.push(node); return true; }
+          next = node[fn]();
+          for(j=next.length-1; j>=0; j--) {
+            node = next[j];
+            if (FindLoop(node.on)) { seen.push(node._element); return true; }
+          }
+        }
+        return false;
+      }
+      return FindLoop(channel);
+    }
+
+    /**
+     * User tells E to listen on channel X
+     *
+     * FindLoop(X) :=
+     * . for all broadcasters B for X:
+     *   . if B eq E => loop found
+     *   . else:
+     *     . for each B:
+     *       . for all listeners L with channel L.Y on B:
+     *         . FindLoop(L.Y)
+     */
+    function findListeningLoop(channel, original) {
+      var seen = [];
+      findDirectionalLoop(channel, original, "reverse", seen);
+      return (seen.length === 0 ? false : seen);
+    }
+
+    /**
+     * User tells E to broadcast on channel X
+     *
+     * FindLoop(X) :=
+     * . for all listeners L for X:
+     *   . if L eq E => loop found
+     *   . else:
+     *     . for each L:
+     *       . for all broadcasts B with channel B.Y on L:
+     *         . FindLoop(B.Y)
+     */
+    function findBroadcastingLoop(channel, original) {
+      var seen = [];
+      findDirectionalLoop(channel, original, "forward", seen);
+      return (seen.length === 0 ? false : seen.reverse());
+    }
+
+    /** Funnel to use the correct FindLoop algorithm
+     *
+     */
+    function findLoop(type, channel, element) {
+      if (type === "listen") {
+        return findListeningLoop(channel, element);
+      }
+      else if (type === "broadcast") {
+        return findBroadcastingLoop(channel, element);
+      }
+      return false;
+    }
+
+    /**
+     * When the "new app" button is pressed, we must make sure to clear the
+     * channel mappings, because they're no longer applicable.
+     */
+    window.addEventListener("resetcards", function clearMap(evt) {
+      maps = {
+        listen: {},
+        broadcast: {}
+      };
+    });
+
+    /**
+     * Remove an element's listen and broadcast records when it leaves the DOM
+     */
+    document.addEventListener("CeciElementRemoved", function forgetElement(evt) {
+      var element = evt.detail, map, pos;
+      Object.keys(maps).forEach(function(type) {
+        map = maps[type];
+        Object.keys(map).forEach(function(channel) {
+          pos = map[channel].indexOf(element);
+          if(pos > -1) {
+            map[channel].splice(pos, 1);
+          }
+        });
+      });
+    });
+
+    /**
+     * Chronicle an element/channel binding when a channel update occurs
+     */
+    window.addEventListener("CeciChannelUpdated", function buildConnectionGraph(evt) {
+      var element = evt.target,
+          data = evt.detail.detail,
+          name = data.name,
+          type = data.type,
+          channel = data.channel,
+          original = data.original,
+          map = maps[type];
+
+      if (!map[channel]) map[channel] = [];
+      if (original) {
+        var pos = map[original].indexOf(element);
+        map[original].splice(pos,1);
+      }
+      if(channel) {
+        var loop = findLoop(type, channel, element);
+        if (loop) { console.warn("Warning: possible loop established. Element chain:", loop); }
+        map[channel].push(element);
+      }
+    });
 
   }
 );
