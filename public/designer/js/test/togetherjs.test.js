db3a139be95ad078126010bf73c9eb3880555ef2
Brian J Brennan <brianloveswords@gmail.com>
2014-09-23 21:18:01 -0400

    Convert designer/js/togetherjs to commonjs

---
+++ public/designer/js/test/togetherjs.test.js
@@ -0,0 +1,6 @@
+var test = require('prova');
+
+test('TogetherJS', function (t) {
+  // TODO: figure out some way to test togetherjs
+  t.end();
+});
--- public/designer/js/togetherjs.js
+++ public/designer/js/togetherjs.js
@@ -2,27 +2,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-define([], function () {
+// TODO: Don't depend on TogetherJS attachment to the global object,
+// explicitly require it.
+var TogetherJS = global.TogetherJS;
 
-  return {
-    init: function (userState) {
-      TogetherJS.on('ready', function () {
-        userState.collaborating = true;
-        TogetherJS.send({type: 'connected'});
-      });
+module.exports = {
+  init: function (userState) {
+    TogetherJS.on('ready', function () {
+      userState.collaborating = true;
+      TogetherJS.send({type: 'connected'});
+    });
 
-      TogetherJS.hub.on('connected', function (e) {
-        TogetherJS.send({type: 'sync', app: document.querySelector('ceci-app').innerHTML})
-      });
+    TogetherJS.hub.on('connected', function (e) {
+      TogetherJS.send({type: 'sync', app: document.querySelector('ceci-app').innerHTML});
+    });
 
-      TogetherJS.hub.on('sync', function (e) {
-        document.querySelector('ceci-app').innerHTML = e.app;
-      });
+    TogetherJS.hub.on('sync', function (e) {
+      document.querySelector('ceci-app').innerHTML = e.app;
+    });
 
-      TogetherJS.on('close', function () {
-        userState.collaborating = false;
-      });
-    }
-  };
-
-});
+    TogetherJS.on('close', function () {
+      userState.collaborating = false;
+    });
+  }
+};
