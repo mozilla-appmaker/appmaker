eaad2b98eda36a2862bdf1377acb520450e65107
Brian J Brennan <brianloveswords@gmail.com>
2014-09-24 12:09:34 -0400

    Convert designer/js/keyboard

--- public/designer/js/keyboard.js
+++ public/designer/js/keyboard.js
@@ -2,32 +2,32 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-define(
-  ["jquery"],
-  function($) {
-    "use strict";
+// TODO: I'm a little uncomfortable with this file being required
+// exclusively for it's side effects -- beyond just feeling wrong to me,
+// testing is harder. If reasonable, we should export a function that
+// takes an element and attaches the event handler.
 
-    $(window).keydown(function(e) {
-      switch (e.keyCode){
-        case 8: // Backspace
-          // Disable backspace if the target ends at the window
-          if ($(e.target).is('body')) {
-            e.preventDefault();
-            console.log('Backspace disabled in keyboard.js');
-          }
-      }
+var $ = require('jquery');
 
-      // Meta key shortcuts:
-      if (e.metaKey) {
-        switch (e.keyCode){
-          case 186: // meta + ;
-            $('input.component-search').val('').focus();
-            break;
-          case 191: // meta + / (shortcut help)
-            $('input.component-search').val('').focus();
-            break;
-        }
-      }
-    });
+$(window).keydown(function(e) {
+  switch (e.keyCode){
+   case 8: // Backspace
+    // Disable backspace if the target ends at the window
+    if ($(e.target).is('body')) {
+      e.preventDefault();
+      console.log('Backspace disabled in keyboard.js');
+    }
   }
-);
+
+  // Meta key shortcuts:
+  if (e.metaKey) {
+    switch (e.keyCode){
+     case 186: // meta + ;
+      $('input.component-search').val('').focus();
+      break;
+     case 191: // meta + / (shortcut help)
+      $('input.component-search').val('').focus();
+      break;
+    }
+  }
+});
---
+++ public/designer/js/test/keyboard.test.js
@@ -0,0 +1,6 @@
+var test = require('prova');
+
+test('keyboard', function (t) {
+  // TODO: figure out how to test.
+  t.end();
+});
