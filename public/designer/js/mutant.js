74112080c3c20de45dc77742efb550b7f239ec13
Brian J Brennan <brianloveswords@gmail.com>
2014-09-25 12:09:39 -0400

    Convert designer/js/mutant

--- public/designer/js/mutant.js
+++ public/designer/js/mutant.js
@@ -16,119 +16,121 @@
  *
  */
 
-define(
-  ["designer/editable"],
-  function(Editable) {
-    "use strict";
+// TODO: while this module does export something, it also has a lot of
+// side effects. It is best practice for a module to not have any side
+// effects when being required.
 
-    //When we show a new card, check to see if there was a selected element on it previously
-    //and select it if there was.
-    window.addEventListener('cardShown', function(){
-      unselectElements();
-      var lastSelected = document.querySelector("ceci-card[visible=true] .last-selected-on-page");
-      if(lastSelected){
-        selectElement(lastSelected);
-      }
-    });
+var Editable = require('./editable');
+var CustomEvent = require('vendor/customevent');
 
-    //If a click happens on the blank app space or on the gray canvas, we deselect all components.
-    window.addEventListener('designerClick', function(e){
-      if(e.detail.classList.contains("container") || e.detail.classList.contains("phone-inner") || e.detail.classList.contains("phone-bottom") || e.detail.tagName === "CECI-APP" ){
-        unselectElements();
-      }
-    });
+// TODO: maybe explicitly require this instead of depending on global scope.
+var Polymer = global.Polymer;
 
-    function selectElement(element){
-      if (element.classList.contains("selected")) {
-        window.dispatchEvent(new CustomEvent("change-tray-tab", { detail: { tab: "customize"}}));
-        return;
-      }
-      unselectElements();
-      element.classList.add('selected');
-
-      //Change the last-selected on this page
-      var thisCard = document.querySelector("ceci-card[visible=true]");
-      var lastSelected = document.querySelector("ceci-card[visible=true] .last-selected-on-page");
-      if(lastSelected){
-        lastSelected.classList.remove("last-selected-on-page");
-      }
-
-      element.classList.add('last-selected-on-page');
-
-      element.onready(function() {
-        Editable.displayAttributes(element);
-      });
-    }
-
-    function unselectElements(){
-      Editable.clearAttributes();
-      var selectedElement = document.querySelector(".brick.selected");
-      if (selectedElement){
-        selectedElement.classList.remove('selected');
-      }
-    }
-
-    function setupElement(element) {
-      if (!element) {
-        return;
-      }
-      element.addEventListener('mousedown', function (e) {
-        selectElement(element);
-      });
-      if (element.classList.contains("selected")) {
-        element.classList.remove("selected");
-        selectElement(element);
-      }
-    }
-
-    // For a special case in Chrome where elements are already in the DOM
-    // and don't fire CeciElementAdded
-
-    function onPolymerReady() {
-      var ceciElements = document.querySelectorAll('ceci-card > *').array();
-      ceciElements.forEach(function (e) {
-        if (e.localName.indexOf('ceci-') === 0) {
-          setupElement(e);
-        }
-      });
-    }
-
-    if (window.Polymer && Polymer.whenPolymerReady) {
-      Polymer.whenPolymerReady(onPolymerReady);
-    } else {
-      window.addEventListener("polymer-ready", onPolymerReady);
-    }
-
-    // Add click handler to new elements
-    window.addEventListener('CeciElementAdded', function(e){
-      setupElement(e.detail);
-    });
-
-    window.addEventListener('CeciElementSelected', function(e){
-      selectElement(e.detail);
-    });
-
-    window.addEventListener('CeciAppDOMReady', function(e){
-      var brick = e.detail.querySelector(".selected.brick");
-      if (!!brick) {
-        brick.classList.remove("selected");
-        selectElement(brick);
-      }
-    });
-
-    window.addEventListener('CeciElementsSorted', function (e){
-      var element = e.detail;
-      if (!element) {
-        return;
-      }
-      element.onready(function() {
-        Editable.displayAttributes(element);
-      });
-    });
-
-    return {
-      selectElement: selectElement
-    };
+//When we show a new card, check to see if there was a selected element on it previously
+//and select it if there was.
+window.addEventListener('cardShown', function(){
+  unselectElements();
+  var lastSelected = document.querySelector("ceci-card[visible=true] .last-selected-on-page");
+  if(lastSelected){
+    selectElement(lastSelected);
   }
-);
+});
 
+//If a click happens on the blank app space or on the gray canvas, we deselect all components.
+window.addEventListener('designerClick', function(e){
+  if(e.detail.classList.contains("container") || e.detail.classList.contains("phone-inner") || e.detail.classList.contains("phone-bottom") || e.detail.tagName === "CECI-APP" ){
+    unselectElements();
+  }
+});
+
+function selectElement(element){
+  if (element.classList.contains("selected")) {
+    window.dispatchEvent(new CustomEvent("change-tray-tab", { detail: { tab: "customize"}}));
+    return;
+  }
+  unselectElements();
+  element.classList.add('selected');
+
+  //Change the last-selected on this page
+  var thisCard = document.querySelector("ceci-card[visible=true]");
+  var lastSelected = document.querySelector("ceci-card[visible=true] .last-selected-on-page");
+  if(lastSelected){
+    lastSelected.classList.remove("last-selected-on-page");
+  }
+
+  element.classList.add('last-selected-on-page');
+
+  element.onready(function() {
+    Editable.displayAttributes(element);
+  });
+}
+
+function unselectElements(){
+  Editable.clearAttributes();
+  var selectedElement = document.querySelector(".brick.selected");
+  if (selectedElement){
+    selectedElement.classList.remove('selected');
+  }
+}
+
+function setupElement(element) {
+  if (!element) {
+    return;
+  }
+  element.addEventListener('mousedown', function (e) {
+    selectElement(element);
+  });
+  if (element.classList.contains("selected")) {
+    element.classList.remove("selected");
+    selectElement(element);
+  }
+}
+
+// For a special case in Chrome where elements are already in the DOM
+// and don't fire CeciElementAdded
+
+function onPolymerReady() {
+  var ceciElements = document.querySelectorAll('ceci-card > *').array();
+  ceciElements.forEach(function (e) {
+    if (e.localName.indexOf('ceci-') === 0) {
+      setupElement(e);
+    }
+  });
+}
+
+if (window.Polymer && Polymer.whenPolymerReady) {
+  Polymer.whenPolymerReady(onPolymerReady);
+} else {
+  window.addEventListener("polymer-ready", onPolymerReady);
+}
+
+// Add click handler to new elements
+window.addEventListener('CeciElementAdded', function(e){
+  setupElement(e.detail);
+});
+
+window.addEventListener('CeciElementSelected', function(e){
+  selectElement(e.detail);
+});
+
+window.addEventListener('CeciAppDOMReady', function(e){
+  var brick = e.detail.querySelector(".selected.brick");
+  if (!!brick) {
+    brick.classList.remove("selected");
+    selectElement(brick);
+  }
+});
+
+window.addEventListener('CeciElementsSorted', function (e){
+  var element = e.detail;
+  if (!element) {
+    return;
+  }
+  element.onready(function() {
+    Editable.displayAttributes(element);
+  });
+});
+
+module.exports = {
+  selectElement: selectElement
+};
---
+++ public/designer/js/test/mutant.test.js
@@ -0,0 +1,6 @@
+var test = require('prova');
+
+test('mutant', function (t) {
+  // TODO: figure out how to test.
+  t.end();
+});
