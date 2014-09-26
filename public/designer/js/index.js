37514189e99158b22034e10df75d1bd0d889f022
Brian J Brennan <brianloveswords@gmail.com>
2014-09-26 12:39:10 -0400

    Use NPM version of webmaker-auth-client.

--- public/designer/js/index.js
+++ public/designer/js/index.js
@@ -24,7 +24,7 @@ var Designer = {
   UserState: require('./userstate'),
   Utils: require('./utils'),
   TogetherJS: require('./togetherjs'),
-  WebmakerAuthClient: require('vendor/webmaker-auth-client')
+  WebmakerAuthClient: require('webmaker-auth-client')
 };
 
 // making things easier for devs of all walks of life
